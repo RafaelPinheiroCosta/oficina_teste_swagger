f4922623590a32a1207a8092f5edd21941eaed18
Rafael Costa <rafaelcost@gmail.com>
2025-09-16 14:48:43 -0300

    melhoria nas validações e testes

--- src/test/java/com/senai/oficina_teste_swagger/ServicoUnitTest.java
+++ src/test/java/com/senai/oficina_teste_swagger/ServicoUnitTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class OficinaTesteSwaggerApplicationTests {
+class ServicoUnitTest {
 
     @Mock
     private ServicoRepository repository;
